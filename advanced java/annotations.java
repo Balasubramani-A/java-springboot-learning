b7f31f3b490b46ff3959446b0e903de4e28d4294
Balasubramani A <balasubramani23bs@gmail.com>
2026-09-03 23:04:48 +0530

    annotations add

---
+++ advanced java/annotations.java
@@ -0,0 +1,19 @@
+// package advanced java;
+class A{
+    public void show(){
+        System.out.println("In A show...");
+    }
+}
+class B extends A{
+    @Override
+    public void show(){
+        System.out.println("In B show.....");
+    }
+}
+
+public class annotations {
+    public static void main(String args[]){
+        B obj = new B();
+        obj.show();
+    }
+}
